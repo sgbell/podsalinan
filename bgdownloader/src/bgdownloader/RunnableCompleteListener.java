063f46490c7cc5ae3bbbae5f8a330310ba1360ae
Sam Bell <bugmanaus@gmail.com>
2011-08-17 23:56:39 +1000

    * Added comment of where code originated from

--- bgdownloader/src/bgdownloader/RunnableCompleteListener.java
+++ bgdownloader/src/bgdownloader/RunnableCompleteListener.java
@@ -1,4 +1,5 @@
-/**
+/** This is from code found at: http://stackoverflow.com/questions/702415/how-to-know-if-other-threads-have-finished
+ *  For proper handling of threads on end of execution.
  * 
  */
 package bgdownloader;
