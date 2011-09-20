cfbabdfcf8cfdde64c0000e49c7d604d1d34ebc2
Sam Bell <bugmanaus@gmail.com>
2011-09-20 21:46:22 +1000

    * Changed the license from GPLv2 to GPLv3. So I can pack sqlite4java with the program

--- bgdownloader/src/podsalinan/DataStorage.java
+++ bgdownloader/src/podsalinan/DataStorage.java
@@ -1,16 +1,21 @@
 /*******************************************************************************
  * Copyright (c) 2011 Sam Bell.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the GNU Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or  any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ * the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * Contributors:
  *     Sam Bell - initial API and implementation
  ******************************************************************************/
-/**
- * 
- */
 package podsalinan;
 
 import java.io.File;
