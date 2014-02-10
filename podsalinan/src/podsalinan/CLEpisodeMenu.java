46963b14d64251ad34a555d0215558a957a66fd2
Sam Bell <bugmanaus@gmail.com>
2014-02-10 13:47:28 +1100

    * Started to code for user ability to change episode status

--- podsalinan/src/podsalinan/CLEpisodeMenu.java
+++ podsalinan/src/podsalinan/CLEpisodeMenu.java
@@ -14,6 +14,7 @@ public class CLEpisodeMenu extends CLMenu {
 	private Episode episode;
 	private Podcast podcast;
 	private URLDownloadList urlDownloads;
+	private CLInput input;
 	
 	/**
 	 * @param newMenuList
@@ -25,45 +26,53 @@ public class CLEpisodeMenu extends CLMenu {
 		String[] menuList = {"1. Download episode",
 							 "2. Delete episode from Drive",
 							 "3. Cancel download of episode",
+							 "4. Change Status",
 							 "",
 							 "9. Return to Podcast menu"};
 		setMainMenuList(menuList);
 		setUrlDownloads(urlDownloads);
-		//TODO: Add Menu Option to Change Episode Status
+		input = new CLInput();
 	}
 	
 	public void printMainMenu(){
 		if (episode!=null){
 			printDetails();
 			super.printMainMenu();
-		}/* else {
-			
-			System.out.println("Error: Invalid Episode");
-			menuList.removeSetting("selectedEpisode");
-		}*/
+		}
 	}
 
 	public void printDetails() {
 		System.out.println ("Podcast: "+podcast.getName());
 		System.out.println ("Episode: "+episode.getTitle());
 		System.out.println ("Date: "+episode.getDate());
+		System.out.println ("Status: "+getStatusString(episode));
+	}
+	
+	public String getStatusString(Episode episode){
+		if (episode==null){
+			episode=this.episode;
+		}
+		String status=null;
+		
 		switch (episode.getStatus()){
 			case Episode.NOT_QUEUED:
-				System.out.println ("Status: Not Downloaded");
+				status = "Not Downloaded";
 				break;
 			case Episode.CURRENTLY_DOWNLOADING:
-				System.out.println ("Status: Downloading Currently");
+				status = "Downloading Currently";
 				break;
 			case Episode.DOWNLOAD_QUEUED:
-				System.out.println ("Status: Download Queued");
+				status = "Download Queued";
 				break;
 			case Episode.INCOMPLETE_DOWNLOAD:
-				System.out.println ("Status: Download Incomplete");
+				status = "Download Incomplete";
 				break;
 			case Episode.FINISHED:
-				System.out.println ("Status: Completed Download");
+				status = "Completed Download";
 				break;
 		}
+		
+		return status;
 	}
 	
 	public Episode getEpisode() {
@@ -105,6 +114,14 @@ public class CLEpisodeMenu extends CLMenu {
 		urlDownloads.addDownload(episode, podcast);
 	}
 
+	/** This method is used to change the episode status, via user input
+	 *  
+	 * @param statusInput
+	 */
+	public void changeStatus(Episode episode, String statusInput) {
+		//TODO: code changeStatus
+	}
+	
 	/**
 	 * @return the podcast
 	 */
@@ -135,6 +152,15 @@ public class CLEpisodeMenu extends CLMenu {
 					// Cancel download of the episode
 					cancelDownloadEpisode();
 					break;
+				case 4:
+				    // Change episode status
+					System.out.println ();
+					
+					//TODO list all status types.
+					System.out.print ("Enter Download Destination["+getStatusString(null)+"]: ");
+					String statusInput=input.getValidNumber(0,7);
+					if (statusInput!=null)
+						changeStatus(null, statusInput);
 				case 9:
 					// Exit episode menu, clearing the selected episode information
 					setEpisode(null,null);
@@ -151,7 +177,7 @@ public class CLEpisodeMenu extends CLMenu {
 		//System.out.println("CLEpisodeMenu.process(int) calling super.process(int)");
 		super.process(userInput);
 	}
-	
+
 	public void process(String userInput){
 		//System.out.println("CLEpisodeMenu.process(String)");
 		super.process(userInput);
