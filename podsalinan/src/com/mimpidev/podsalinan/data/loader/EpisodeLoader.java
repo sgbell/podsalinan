3fd0a868b7d3fc5c7be9ca0770fa7e7f2819c75c
Sam Bell <bugmanaus@gmail.com>
2014-10-13 02:55:10 +0000

    * Added a new class

---
+++ podsalinan/src/com/mimpidev/podsalinan/data/loader/EpisodeLoader.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.mimpidev.podsalinan.data.loader;
+
+/**
+ * @author sbell
+ *
+ */
+public class EpisodeLoader extends TableLoader {
+
+	/**
+	 * 
+	 */
+	public EpisodeLoader() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
