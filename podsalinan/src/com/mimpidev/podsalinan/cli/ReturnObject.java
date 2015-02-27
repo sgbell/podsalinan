245ff36fda8cb816a06c43d7549e6eff6b3bb50a
Sam Bell <bugmanaus@gmail.com>
2015-02-27 02:27:42 +0000

    * added brackets, no major change

--- podsalinan/src/com/mimpidev/podsalinan/cli/ReturnObject.java
+++ podsalinan/src/com/mimpidev/podsalinan/cli/ReturnObject.java
@@ -42,7 +42,7 @@ public class ReturnObject {
 	public boolean execute=false;
 
 	public boolean useGlobalSelection() {
-		if (execute && methodCall.length()<1)
+		if ((execute) && (methodCall.length()<1))
 			return true;
 		else
 			return false;
