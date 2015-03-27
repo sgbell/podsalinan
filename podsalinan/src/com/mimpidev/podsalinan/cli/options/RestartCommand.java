6c249174db2c9b98d0929e2b34f629adc5b993e4
Sam Bell <bugmanaus@gmail.com>
2015-03-27 02:16:19 +0000

    * RestartCommand Body completed

--- podsalinan/src/com/mimpidev/podsalinan/cli/options/RestartCommand.java
+++ podsalinan/src/com/mimpidev/podsalinan/cli/options/RestartCommand.java
@@ -4,7 +4,9 @@
 package com.mimpidev.podsalinan.cli.options;
 
 import com.mimpidev.podsalinan.DataStorage;
+import com.mimpidev.podsalinan.Podsalinan;
 import com.mimpidev.podsalinan.cli.CLIOption;
+import com.mimpidev.podsalinan.cli.CLInterface;
 import com.mimpidev.podsalinan.cli.ReturnObject;
 
 /**
@@ -22,22 +24,25 @@ public class RestartCommand extends CLIOption {
 
 	@Override
 	public ReturnObject execute(String command) {
-		/*TODO: flesh out RestartCommand
-		 * 
-		 */
-		/*menuInput = menuInput.replaceFirst(menuInput.split(" ")[0]+" ", "");
-		if (((menuInput.equalsIgnoreCase("delete"))||(menuInput.equalsIgnoreCase("episode")))&&
-			(menuList.get(menuList.size()-1).name.equalsIgnoreCase("selectedEpisode"))){
-			CLEpisodeMenu episodeMenu = (CLEpisodeMenu)mainMenu.findSubmenu("episode_selected");
-			episodeMenu.deleteEpisodeFromDrive();
-			System.out.println("Deleting file for episode: "+episodeMenu.getEpisode().getTitle());
-		} else if (menuList.get(menuList.size()-1).name.equalsIgnoreCase("downloads")){
-			if (menuList.get(menuList.size()-1).name.equalsIgnoreCase("selectedDownload")){
-				URLDownload download = ((CLDownloadSelectedMenu)mainMenu.findSubmenu("downloadSelected_menu")).getDownload();
-				data.getUrlDownloads().deleteDownload(data.getUrlDownloads().findDownload(download.getURL()));
-				System.out.println("Deleting file for download: "+download.getURL().toString());
+		debug=true;
+		if (debug) Podsalinan.debugLog.logInfo(this,"command: "+command);
+		//TODO: flesh out RestartCommand
+		
+		String[] commandSplit = command.split(" ");
+		if (debug) Podsalinan.debugLog.logInfo(this, "size: "+commandSplit.length);
+		String downloadUid="";
+		//Check length of command
+		if (command.length()==0 && CLInterface.cliGlobals.getGlobalSelection().containsKey("downloads")){
+			downloadUid=CLInterface.cliGlobals.getGlobalSelection().get("downloads");
+		} else {
+			if (data.getUrlDownloads().findDownloadByUid(commandSplit[0])!=null){
+				downloadUid=commandSplit[0];
 			}
-		}*/
+		}
+		if (downloadUid!=""){
+			data.getUrlDownloads().restartDownload(downloadUid);
+		}
+		
 		return returnObject;
 	}
 
