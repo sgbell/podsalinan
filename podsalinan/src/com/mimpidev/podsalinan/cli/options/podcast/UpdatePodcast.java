e8bbf5daf09a507c34d51b7e0269f500390a23bc
Sam Bell <bugmanaus@gmail.com>
2015-08-23 08:15:31 +1000

    * UpdatePodcast - fixed it so it's working

--- podsalinan/src/com/mimpidev/podsalinan/cli/options/podcast/UpdatePodcast.java
+++ podsalinan/src/com/mimpidev/podsalinan/cli/options/podcast/UpdatePodcast.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import com.mimpidev.podsalinan.DataStorage;
 import com.mimpidev.podsalinan.Podsalinan;
 import com.mimpidev.podsalinan.cli.CLIOption;
+import com.mimpidev.podsalinan.cli.CLInterface;
 import com.mimpidev.podsalinan.cli.ReturnObject;
 import com.mimpidev.podsalinan.data.Podcast;
 
@@ -24,26 +25,27 @@ public class UpdatePodcast extends CLIOption {
 		super(newData);
 	}
 
-	/* (non-Javadoc)
-	 * @see com.mimpidev.podsalinan.cli.CLIOption#execute(java.lang.String)
-	 */
 	@Override
 	public ReturnObject execute(Map<String, String> functionParms) {
-		String command="";
-		debug=true;
-		if (debug) Podsalinan.debugLog.logInfo("["+getClass().getName()+"]");
+		if (debug) Podsalinan.debugLog.logMap(this, functionParms);
 		
 		String tempDir=data.getSettingsDir();
 		if (debug) Podsalinan.debugLog.logInfo(this, "tempDir:"+tempDir);
+		Podcast selectedPodcast = null;
 		
-		Podcast selectedPodcast = data.getPodcasts().getPodcastByUid(command.split(" ")[0]);
+        if (functionParms.containsKey("uid"))
+        	selectedPodcast = data.getPodcasts().getPodcastByUid(functionParms.get("uid"));
+        if (selectedPodcast==null && CLInterface.cliGlobals.getGlobalSelection().containsKey("podcastid")){
+        	selectedPodcast = data.getPodcasts().getPodcastByUid(CLInterface.cliGlobals.getGlobalSelection().get("podcastid"));
+        }
 		if (selectedPodcast!=null){
-			if (debug) Podsalinan.debugLog.logInfo(this, "Updating Podcast");
+			System.out.println("Updating Podcast.");
 			selectedPodcast.updateList(tempDir, true);
+			returnObject.methodCall = "podcast "+selectedPodcast.getDatafile();
+		} else {
+			System.out.println("Error: Invalid podcast passed to command");
+			returnObject.methodCall = "podcast showmenu";
 		}
-		returnObject = new ReturnObject();
-		returnObject.methodCall = "podcast";
-		//returnObject.methodParameters = command.split(" ")[0];
 		returnObject.execute=true;
 		
 		return returnObject;
