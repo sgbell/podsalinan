f202c0be1d77bad2425987a39db72ddf9cd89d47
Sam Bell <bugmanaus@gmail.com>
2015-09-16 03:11:21 +0000

    * Fixed adding orderby to the sql statement, if not defined

--- podsalinan/src/com/mimpidev/sql/sqlitejdbc/cursors/SqliteOrderCursor.java
+++ podsalinan/src/com/mimpidev/sql/sqlitejdbc/cursors/SqliteOrderCursor.java
@@ -16,7 +16,7 @@ public class SqliteOrderCursor extends SqliteCursor {
 	public SqliteOrderCursor(Database db, SqliteDataTable table, String orderby) throws SqliteException {
 		super(db, table);
 		String sqlStatement = "SELECT * FROM "+table.getName();
-		if (orderby != null){
+		if (orderby != null && !orderby.equals("")){
 		   sqlStatement+=" ORDER BY "+orderby;
 		}
 		executeSql(sqlStatement);
